138a429b6432cd0282149564f9a9b454eabb088c
MuhammadAqibRafiq <77021708+MuhammadAqibRafiq@users.noreply.github.com>
2021-11-16 14:45:24 +0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAb0p2l9Ntf3AhFppAa7DpvUIGl9kqHsvU",
-    authDomain: "diary-dc615.firebaseapp.com",
-    projectId: "diary-dc615",
-    storageBucket: "diary-dc615.appspot.com",
-    messagingSenderId: "659105278765",
-    appId: "1:659105278765:web:d11b8deab0428b8f1901a4"
-};
+    apiKey: "AIzaSyBAvMomDIZI8lD8KoNPZ1tWwIe5arZ8mkg",
+    authDomain: "linkedin-88fed.firebaseapp.com",
+    projectId: "linkedin-88fed",
+    storageBucket: "linkedin-88fed.appspot.com",
+    messagingSenderId: "219687867990",
+    appId: "1:219687867990:web:5e5a2eedba24c256a20df7"
+  };
 firebase.initializeApp(firebaseConfig)
 
 const auth = firebase.auth();
